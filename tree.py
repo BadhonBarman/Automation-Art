c770055ae9df000218221f835082c1b3dd0564b7
Badhon Barman <58289031+BadhonBarman@users.noreply.github.com>
2020-06-26 02:29:48 -0700

    Add files via upload

---
+++ tree.py
@@ -0,0 +1,27 @@
+
+
+import turtle 
+
+t=turtle.Turtle()
+turtle.bgcolor("black")
+t.pencolor("white")
+t.left(90)
+t.speed(1000)
+def tree(i):
+	if i<10:
+		return
+	else:
+
+		t.forward(i)
+		t.left(30)
+		tree(3*i/4)
+		t.right(60)
+		tree(3*i/4)
+		t.left(30)
+		t.backward(i)
+
+
+
+tree(100)
+turtle.done()
+	
