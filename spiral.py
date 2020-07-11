095fc41c082f370d57fc9ce54a77d2798154eda8
Badhon Barman <58289031+BadhonBarman@users.noreply.github.com>
2020-07-11 20:20:31 +0600

    Update spiral.py

--- spiral.py
+++ spiral.py
@@ -1,21 +1,5 @@
-'''
+
 from turtle import *
-
-#import turtle 
-color('red', 'yellow')
-setup(500,500)
-
-tl = Turtle()
-
-tl.hideturtle()
-
-for i in range(250):
-	tl.forward(i)
-	tl.right(50)
-	tl.speed(0)
-
-done()
-'''
 
 import turtle
 
@@ -25,13 +9,11 @@ t = turtle.Pen()
 turtle.bgcolor('black')
 
 for x in range(300):
-	t.pencolor(colors[x%6])
-	t.width(x/100+1)
+	t.pencolor(colors[x%5])
+	t.width(x/100)
 	t.forward(x)
-	t.left(91)
+	t.left(61)
 	t.speed(10000000000)
 
 turtle.done()
 
-
-#4.43 😔
